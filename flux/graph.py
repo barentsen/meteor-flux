d1e7268834075538752aa88b7c7401e282a7df64
Geert Barentsen <geert@barentsen.be>
2013-08-18 14:35:54 +0100

    initial commit of util and graph

---
+++ flux/graph.py
@@ -0,0 +1,320 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Plot flux graphs."""
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import datetime
+import tempfile
+from astropy import log
+from astropy.time import Time
+import os
+
+import util
+
+TMP_LOCAL = '/tmp'
+TMP_PUBLIC = 'http://tbd/'
+
+class VideoFluxProfile(object):
+
+    def __init__(self, fluxdb,
+                 shower, start, stop,
+                 min_interval=1, max_interval=24,
+                 min_meteors=20, min_eca=20, 
+                 min_alt=10, min_eca_station=0.5,
+                 gamma=1.5, popindex=2.0):
+        """
+
+        Parameters
+        ----------
+        shower : string
+            IMO shower code
+
+        start : string
+            ISO timestamp
+
+        stop : string
+            ISO timestamp
+
+        min_meteors : int
+            Minimum number of meteors in each bin.
+
+        min_eca : float [10^3 km^2 h]
+            Minimum ECA in each bin.
+
+        min_interval : float [hours]
+
+        max_interval : float [hours]
+
+        min_alt : float [degrees]
+            Minimum radiant altitude for a flux record to be included.
+
+        min_eca_station : float [degrees]
+            Minimum ECA for a flux record to be included.
+
+        gamma : float
+            Zenith correction exponent.
+
+        popindex : float
+            Population index.
+
+        Returns
+        -------
+        Result of the query.
+        """
+        self.start = Time(start, scale='utc')
+        self.stop = Time(stop, scale='utc')
+        self.popindex = popindex
+        self.gamma = gamma
+        self.fluxes = fluxdb.query("""SELECT * FROM
+                                    bin_adaptive(%s,
+                                                 %s::timestamp,
+                                                 %s::timestamp,
+                                                 %s, %s,
+                                                 '%s hours'::interval,
+                                                 '%s hours'::interval,
+                                                 %s, %s, %s, %s)
+                                """, (shower, 
+                                      start, 
+                                      stop,
+                                      min_meteors, min_eca,
+                                      min_interval, max_interval,
+                                      min_alt, min_eca_station,
+                                      gamma, popindex, ))
+
+    def field(self, key):
+        return np.array([row[key] for row in self.fluxes])
+
+    def get_json(self):
+        graph_filename = self.get_graph()
+        if self.fluxes is None or len(self.fluxes) == 0:
+            log.error('Could not find fluxes')
+            json = '{"status":"ERROR"}'
+        else:
+            json = '{{"status":"OK", "graph":"{0}", "flux": ['.format(graph_filename)
+            for i, row in enumerate(self.fluxes):
+                if i > 0:
+                    json += ', \n'
+                json += '{'
+                json += '"time":"{0}", '.format(str(row['time'])[0:16])
+                json += '"sollon":{0:.3f}, '.format(row['solarlon'])
+                json += '"teff":{0:.1f}, '.format(row['teff'])
+                json += '"eca":{0:.1f}, '.format(row['eca'])
+                json += '"met":{0}, '.format(row['met'])
+                json += '"flux":{0:.1f}, '.format(row['flux'])
+                json += '"e_flux":{0:.1f}, '.format(row['e_flux'])
+                json += '"zhr":{0:.0f}'.format(row['zhr'])
+                json += '}'
+            json += ']}'
+        
+        return json
+
+    def get_graph(self):
+        graph = VideoFluxGraph(self)
+        return graph.save()
+
+
+
+
+
+class BaseGraph(object):
+
+    def __init__(self):
+        self.fig = plt.figure(figsize=(11,6), dpi=80) # 11*80 = 880 pixels wide!
+        self.fig.subplots_adjust(0.1,0.17,0.92,0.87)
+
+    def show(self):
+        self.fig.show()        
+        
+    def save(self, prefix='flux', format='png', dpi=100, tmpdir=TMP_LOCAL, web=True):
+        myfile = tempfile.NamedTemporaryFile(prefix=prefix,
+                                             suffix='.'+format,
+                                             dir=tmpdir,
+                                             delete=False)
+        self.fig.savefig(myfile, format=format, dpi=dpi)
+        
+        if web:
+            return TMP_PUBLIC+os.path.basename(myfile.name)
+            myfile.close()
+            plt.close()
+        else:
+            return myfile
+
+
+class VideoFluxGraph(BaseGraph):
+
+    def __init__(self, profile, ymax=None):
+        BaseGraph.__init__(self)
+        self.profile = profile
+        self.timespan = (self.profile.stop - self.profile.start).sec / 3600. # hours
+        self.ymax = ymax
+
+    
+    def plot(self):
+        self.setup_axes()
+
+        self.ax.errorbar(self.profile.field('time'),
+                         self.profile.field('flux'),
+                         yerr=self.profile.field('e_flux'),
+                         fmt="s", ms=4, lw=1.0, c='red')
+        
+        self.setup_limits()
+        self.setup_time_axes()
+        self.setup_labels()
+       
+
+    def setup_axes(self):
+        self.ax = plt.subplot(111)
+        
+        # Second Y axis on the right for the ZHR 
+        self.ax_zhr = plt.twinx(ax=self.ax)
+        self.ax_zhr.set_ylabel("ZHR (r=%.1f)" % self.profile.popindex, fontsize=16)
+        self.ax_zhr.yaxis.set_major_formatter(plt.FuncFormatter(self.zhr_formatter))
+        
+        # Second X axis as top for the solar longitude
+        self.ax2 = plt.twiny(ax=self.ax)
+        self.ax2.set_xlabel("Solar longitude (J2000.0)", fontsize=16)
+        self.ax2.xaxis.set_major_formatter(plt.FuncFormatter(self.sollon_formatter))
+          
+        self.ax.grid(which="both")
+
+    def setup_limits(self):
+        self.ax.set_xlim([self.profile.start.datetime, self.profile.stop.datetime])
+        self.ax2.set_xlim([self.profile.start.datetime, self.profile.stop.datetime])
+        self.ax_zhr.set_xlim([self.profile.start.datetime, self.profile.stop.datetime])
+
+        flux = self.profile.field('flux')
+        e_flux = self.profile.field('e_flux')
+        # Determine the limit of the Y axis
+        if self.ymax:
+            my_ymax = self.ymax
+        elif len(flux) == 0:
+            my_ymax = 100
+        else:
+            my_ymax = 1.1*max(flux+e_flux)
+
+        self.ax.set_ylim([0, my_ymax])
+        self.ax2.set_ylim([0, my_ymax])
+        self.ax_zhr.set_ylim([0, my_ymax])      
+
+
+    def setup_time_axes(self):
+        if self.timespan > 90*24: # 90 days
+            # More than 5 days: only show dates
+            majorLocator = mpl.dates.AutoDateLocator(maxticks=10)
+            sollonLocator = majorLocator
+            majorFormatter = mpl.dates.DateFormatter('%d %b')
+            self.xlabel = "Date (UT, %s)" % self.profile.start.datetime.year   
+                    
+        elif self.timespan > 5*24: # 5 days
+            # More than 5 days: only show dates
+            majorLocator = mpl.dates.AutoDateLocator(maxticks=10)
+            sollonLocator = majorLocator
+            majorFormatter = mpl.dates.DateFormatter('%d %b')
+            self.xlabel = "Date (UT, %s)" % self.profile.start.datetime.year   
+        
+        elif self.timespan > 1*24:
+            # Between 1 and 5 days: show hours
+            majorLocator = mpl.dates.HourLocator(byhour=[0])
+            majorFormatter = mpl.dates.DateFormatter('%d %b')
+            
+            if self.timespan > 3*24:
+                t = 12
+            elif self.timespan > 1.5*24:
+                t = 6
+            else:
+                t = 3
+            byhour = np.arange(t, 24, t)
+            
+            minorLocator = mpl.dates.HourLocator(byhour=byhour)
+            self.ax.xaxis.set_minor_locator(minorLocator)
+            sollonLocator = mpl.dates.HourLocator(byhour=np.append(0, byhour))
+            fmt2 = mpl.dates.DateFormatter('%H:%M')        
+            self.ax.xaxis.set_minor_formatter(plt.FuncFormatter(fmt2))
+            self.xlabel = "Date (UT, %s)" % self.profile.start.datetime.year   
+            
+        else:
+            if self.timespan > 18:
+                majorLocator = mpl.dates.HourLocator( np.arange(0, 24, 3) ) 
+                minorLocator = mpl.dates.HourLocator( np.arange(0, 24, 1) ) 
+            elif self.timespan > 12:
+                majorLocator = mpl.dates.HourLocator( np.arange(0, 24, 2) ) 
+                minorLocator = mpl.dates.HourLocator( np.arange(1, 24, 2) ) 
+            elif self.timespan > 6:
+                majorLocator = mpl.dates.HourLocator( np.arange(0, 24, 1) ) 
+                minorLocator = mpl.dates.MinuteLocator(  np.arange(0,60,30)  )
+            elif self.timespan > 3:
+                majorLocator = mpl.dates.MinuteLocator(  np.arange(0,60,30)  )
+                minorLocator = mpl.dates.MinuteLocator(  np.arange(0,60,15)  )
+            elif self.timespan > 1:
+                majorLocator = mpl.dates.MinuteLocator( np.arange(0,60,15) ) 
+                minorLocator = mpl.dates.MinuteLocator(  np.arange(0,60,5) )
+            else:
+                majorLocator = mpl.dates.MinuteLocator( np.arange(0,60,10) ) 
+                minorLocator = mpl.dates.MinuteLocator(  np.arange(0,60,2) )
+                
+            
+            majorFormatter = mpl.dates.DateFormatter('%H:%M')  
+            self.ax.xaxis.set_minor_locator(minorLocator)
+            sollonLocator = majorLocator
+            self.xlabel = "Time (UT, %s)" % self.profile.start.datetime.strftime('%d %b %Y')
+        
+        self.ax.xaxis.set_major_formatter(plt.FuncFormatter(majorFormatter))    
+        self.ax2.xaxis.set_major_locator(sollonLocator)
+        self.ax.xaxis.set_major_locator(majorLocator)
+
+
+    def setup_labels(self):
+        self.ax.set_ylabel("Meteoroids / 1000$\cdot$km$^{2}\cdot$h", fontsize=18)
+        self.ax.set_xlabel(self.xlabel, fontsize=18)
+        
+        labels = self.ax.get_xmajorticklabels()
+        plt.setp(labels, rotation=45, fontsize=14) 
+        labels = self.ax.get_xminorticklabels()
+        plt.setp(labels, rotation=45, fontsize=12)
+
+
+    ######################
+    # FORMATTER FUNCTIONS
+    ######################
+
+    def sollon_formatter(self, a, b):
+        """
+        a: ordinal datetime
+        b: tick nr 
+        Usage: ax.xaxis.set_major_formatter(pylab.FuncFormatter(sollon_formatter)) 
+        """
+        # 10 days
+        if self.timespan > 10*24*3600:
+            fmt = "%.1f"
+        # 1 day
+        elif self.timespan > 24*3600:
+            fmt = "%.2f"
+        else:
+            fmt = "%.3f"
+        d = datetime.datetime.fromordinal(int(a))+datetime.timedelta(a-int(a))
+        return fmt % util.sollon(d)
+
+    def date_formatter(self, a, b):
+        """
+        a: ordinal datetime
+        b: tick nr 
+        Usage: ax.xaxis.set_major_formatter(pylab.FuncFormatter(sollon_formatter)) 
+        """
+        d = datetime.datetime.fromordinal(int(a))+datetime.timedelta(a-int(a))
+        if self.timespan > 56:
+            fmt = '%d %b'
+        else:
+            fmt = '%H:%M\n(%d %b)'
+        
+        return d.strftime(fmt)
+
+    def zhr_formatter(self, a, b):
+        zhr = util.flux2zhr(a, self.profile.popindex)
+        if round(zhr) < 10:
+            return "%.1f" % zhr
+        else:
+            return "%.0f" % zhr
+
+
+
---
+++ flux/util.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Utility functions."""
+import numpy as np
+import math as m
+import datetime
+
+def flux2zhr(flux, pop_index=2.0):
+    """Eqn 41 from (Koschak 1990b)
+
+    Parameters
+    ----------
+    flux : float [1000^-1 km^-2 h^-2]
+    """    
+    r = pop_index
+    flux = flux / 1000.0
+    zhr = (flux * 37200.0) / ( (13.1*r - 16.45) * (r - 1.3)**0.748 )
+    return zhr
+
+
+def jd(mydate):
+    """Convert a Gregorian date/time in Universal Time to the corresponding Julian Day.
+    This is the number of days since Greenwich noon of January 1, 4713 B.C.
+    Algorithm from 'Astronomical Algorithms', Jean Meeus, p61.
+    
+    Parameters
+    ----------
+    mydate : Python DateTime object (UT) to convert
+    
+    Returns
+    -------
+    Decimal Julian Day (float)
+    
+    Examples
+    --------
+    >>> jd(datetime.datetime(2000, 1, 1, 12, 0, 0))
+    2451545.0
+
+    >>> jd(datetime.datetime(2000, 1, 1, 12, 0, 0, int(1E5)))
+    2451545.0000011576
+    """
+    # Get the day expressed as a decimal number
+    year = mydate.year
+    month = mydate.month
+    day_decimal = mydate.day + (mydate.hour / 24.0) + (mydate.minute / 1440.0) + ( (mydate.second + (mydate.microsecond/1E6)) / 86400.0)
+    # In this algorithm, January and February have to be considered as "13th and 14th month"
+    if (month == 1 or month == 2):
+        month = month + 12
+        year = year - 1 
+    # Calculate A and B
+    A = np.floor( year / 100.0 )
+    B = 2 - A + np.floor( A / 4.0 )
+    # Calculate the Julian Day
+    result = np.floor(365.25 * (year + 4716)) + np.floor(30.6001 * (month + 1)) + day_decimal + B - 1524.5
+    return result
+
+
+def sollon(datetime):
+    """Returns the solar longitude in decimal degrees.
+
+    Calculation of the solar longitude with an accuracy of about .003 deg
+    Algorithm based on Jean Meeus' "Astronomical Algorithms" and an article by C. Steyaert in WGN
+    Parameter 1: timestamp
+    Returns: solar longitude in decimal degrees
+    
+    Original version: 1995 Jan 28 Rainer Arlt, translated to plpgsql by Geert Barentsen in 2004
+    """
+    # Numbers from "Astronomical Algorithms" (Jean Meeus) pp 205
+    a0 = [334166.0, 3489.0, 350.0, 342.0, 314.0, 268.0, 234.0, 132.0, 127.0, 120.0, 99.0, 90.0, 86.0, 78.0, 75.0, 51.0, 49.0, 36.0, 32.0, 28.0, 27.0, 24.0, 21.0, 21.0, 20.0, 16.0, 13.0, 13.0]
+    b0 = [4.669257, 4.6261, 2.744, 2.829, 3.628, 4.418, 6.135, 0.742, 2.037, 1.11, 5.233, 2.045, 3.508, 1.179, 2.533, 4.58, 4.21, 2.92, 5.85, 1.90, 0.31, 0.34, 4.81, 1.87, 2.46, 0.83, 3.41, 1.08]
+    c0 = [6283.07585, 12566.1517, 5753.385, 3.523, 77713.771, 7860.419, 3930.210, 11506.77, 529.691, 1577.344, 5884.927, 26.298, 398.149, 5223.694, 5507.553, 18849.23, 775.52, 0.07, 11790.63, 796.30, 10977.08, 5486.78, 2544.31, 5573.14, 6069.78, 213.30, 2942.46, 20.78]
+    a1 = [20606.0, 430.0, 43.0]
+    b1 = [2.67823, 2.635, 1.59]
+    julian = jd(datetime)
+    T = (julian - 2451545.0) / 365250.0
+    result = 4.8950627 + T * (6283.0758500 - T * 0.0000099)
+
+    # Calculate s0
+    s0 = 0.0
+    for n in range(len(b0)):
+        angle = b0[n] + c0[n] * T
+        s0 = s0 + a0[n] * np.cos(angle)
+
+    # Calculate s1
+    s1 = 0.0
+    for n in range(3):
+        angle = b1[n] + c0[n] * T
+        s1 = s1 + a1[n] * np.cos(angle)
+
+    # Calculate s2
+    angle = 1.073 + c0[0] * T
+    angle1 = 0.44 + c0[1] * T
+    s2 = 872.0 * np.cos(angle) + 29 * np.cos(angle1)
+    
+    # Calculate s3
+    angle = 5.84 + c0[0] * T
+    s3 = 29.0 * np.cos(angle)
+    
+    # The required longitude in radians is given by:
+    result = result + ( s0 + T * ( s1 + T * ( s2 + T * s3 ) ) ) * 1.0e-7
+    
+    # Normalize the angle
+    while result > 2.0*np.pi:
+        result = result - 2.0*np.pi
+    while result < 0:
+        result = result + 2.0*np.pi
+    
+    # Return the result (DEGREES!)
+    return np.degrees(result)
+
