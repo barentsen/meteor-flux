cb4e23dc2f90a5f7728518651f1c977631d372bd
Geert Barentsen <geert@barentsen.be>
2013-08-19 16:16:12 +0000

    bugfixes

--- scripts/service.py
+++ scripts/service.py
@@ -8,4 +8,4 @@ from flux.app import fluxapp
 
 if __name__ == "__main__":
     fluxapp.debug = True
-    fluxapp.run(port=8042, host='0.0.0.0')
+    fluxapp.run(port=8042, host='0.0.0.0', processes=3)
