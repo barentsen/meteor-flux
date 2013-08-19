0aae06cd8a6338d30196433023026f9e8614ae93
Geert Barentsen <geert@barentsen.be>
2013-08-19 23:55:41 +0100

    -

--- flux/graph.py
+++ flux/graph.py
@@ -46,10 +46,7 @@ class BaseGraph(object):
                                              delete=False)
         self.fig.savefig(myfile, format=format, dpi=dpi)
         
-        myfile2 = tempfile.NamedTemporaryFile(prefix=prefix,
-                                             suffix='.pdf',
-                                             dir=tmpdir,
-                                             delete=False)
+        myfile2 = myfile.replace('png', 'pdf')
         self.fig.savefig(myfile2, format='pdf', dpi=dpi)
 
         if web:
