c64e5bc4005dd3ea14671ce69f241c5f4ab9c733
Geert Barentsen <geert@barentsen.be>
2013-08-19 14:09:34 +0100

    renamed app to fluxapp

--- scripts/service.py
+++ scripts/service.py
@@ -2,10 +2,8 @@
 # -*- coding: utf-8 -*-
 """Starts the Flask app in debug mode.
 """
-from flux import app
+from flux.app import fluxapp
 
 if __name__ == "__main__":
-    port = 8042
-    # Set up the development server on port 8000.
-    app.debug = True
-    app.run(port=port)
+    fluxapp.debug = True
+    fluxapp.run(port=8042)
