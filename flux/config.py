8258abcf050320ec77bcbbe9c9b32900eb1ea0f0
Geert Barentsen <geert@barentsen.be>
2013-08-19 14:07:01 +0100

    ensure agg backend

--- flux/config.py
+++ flux/config.py
@@ -6,11 +6,16 @@ import os
 PACKAGEDIR = os.path.dirname(os.path.abspath(__file__))
 
 HOSTNAME = os.uname()[1]
-if HOSTNAME == 'gvm':  # testing machine
+if HOSTNAME == 'gvm':
     DEBUG = True
+    DBINFO = 'dbname=fluxdb user=postgres'
     TMPDIR = '/home/gb/dev/meteor-flux/tmp'
     TMPDIR_WWW = '/meteor-flux/tmp'
+if HOSTNAME == 'flux':  # testing machine
+    DEBUG = False
     DBINFO = 'dbname=fluxdb user=postgres'
+    TMPDIR = '/tmp'
+    TMPDIR_WWW = '/tmp'
 else:
     DEBUG = False
     DBINFO = 'host=flux.geert.io dbname=fluxdb user=postgres'
