a5e52c2d36fdeab773fec9c163c209c1b2be9a1e
Geert Barentsen <geert@barentsen.be>
2013-08-19 14:45:10 +0100

    Listen on all IP during debug

--- scripts/service.py
+++ scripts/service.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Starts the Flask app in debug mode.
+
+Do not use in production.
 """
 from flux.app import fluxapp
 
 if __name__ == "__main__":
     fluxapp.debug = True
-    fluxapp.run(port=8042)
+    fluxapp.run(port=8042, host='0.0.0.0')
