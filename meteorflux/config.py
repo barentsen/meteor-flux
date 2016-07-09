c67bf39783e46ce5218574579cc1f01e9b9bd8e7
Geert Barentsen <geert@barentsen.be>
2016-07-09 20:40:20 +0000

    Remove old configurations

--- meteorflux/config.py
+++ meteorflux/config.py
@@ -4,20 +4,9 @@ import os
 PACKAGEDIR = os.path.dirname(os.path.abspath(__file__))
 
 HOSTNAME = os.uname()[1]
-if HOSTNAME == 'gvm':
+if HOSTNAME == 'ec.geert.io' or HOSTNAME == 'imo.geert.io' or HOSTNAME == 'meteorflux.io':
     DEBUG = True
-    DBINFO = 'host=flux.geert.io dbname=fluxdb user=postgres'
-    #DBINFO = 'dbname=fluxdb user=postgres'
-    TMPDIR = '/tmp'
-    TMPDIR_WWW = '/tmp'
-elif HOSTNAME == 'flux':  # testing machine
-    DEBUG = False
-    DBINFO = 'dbname=fluxdb user=postgres'
-    TMPDIR = '/mnt/fluxtmp'
-    TMPDIR_WWW = '/tmp'
-elif HOSTNAME == 'ec.geert.io' or HOSTNAME == 'imo.geert.io' or HOSTNAME == 'meteorflux.io':
-    DEBUG = True
-    DBINFO = 'host=/var/run/postgresql dbname=fluxdb user=postgres'
+    DBINFO = 'host=/var/run/postgresql dbname=flux_kca user=postgres'
     TMPDIR = '/var/www/tmp'
     TMPDIR_WWW = '/tmp'
 else:
@@ -31,3 +20,4 @@ else:
 DBINFO_TESTING = 'host=/var/run/postgresql dbname=testdb user=postgres'
 DPI = 80  # Default DPI of graphs
 MARKERS = ['s', '^', 'o', 's', '^', 'o', 's', '^', 'o', 's', '^', 'o']
+
